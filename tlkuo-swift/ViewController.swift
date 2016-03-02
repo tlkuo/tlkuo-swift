0c13815652e67028ed6f9402094b023ba4beb559
郭宗麟 <tlkuo.frb@gmail.com>
2016-03-02 22:34:27 +0800

    Initial swift project

---
+++ tlkuo-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tlkuo-swift
+//
+//  Created by martin on 2016/3/2.
+//  Copyright © 2016年 Frainbow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
