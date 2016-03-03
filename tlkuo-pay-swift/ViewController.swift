948d7aa2711cc431a346741834b0161a9749ee73
martin <tlkuo.frb@gmail.com>
2016-03-03 21:50:47 +0800

    Add target of pay version

---
+++ tlkuo-pay-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tlkuo-pay-swift
+//
+//  Created by martin on 2016/3/3.
+//  Copyright © 2016年 Frainbow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
